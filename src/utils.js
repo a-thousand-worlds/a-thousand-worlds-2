479db9bf41926f138c281acce2bcf79479826839
abrakadobr <abrakadobr@gmail.com>
2020-12-01 02:36:18 +0700

    dirty fix of node-isbn - duplicate search on browser side if server side failed; +npm run deploy shortcut

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,27 @@
 import axios from 'axios'
+import isbn from 'node-isbn'
 
 const FNURL = process.env.VUE_APP_SEARCH_SERVICE_URL
 
+function _isbn(code, provider) {
+  return new Promise((resolve, reject) => {
+    isbn
+      .provider([provider])
+      .resolve(code, (err, book) => {
+        if (book && !err) {
+          resolve(book)
+          return
+        }
+        resolve(null)
+      })
+  })
+}
+
 export async function isbnSearch(code) {
   const req = await axios.get(FNURL + '?isbn=' + code)
-  return req.data
+  const ret = req.data
+  if (!ret.google) {
+    ret.google = await _isbn(code, 'google')
+  }
+  return ret
 }
