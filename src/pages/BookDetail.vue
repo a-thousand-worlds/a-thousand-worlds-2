be99beb628df99d79934cda70e2dbe7a5ac513ba
Raine Revere <raine@cybersemics.org>
2020-12-01 21:34:44 -0700

    BookDetail: basic layout and content

--- src/pages/BookDetail.vue
+++ src/pages/BookDetail.vue
@@ -1,7 +1,13 @@
 <script>
+import AuthorWidget from '@/components/AuthorWidget'
+import BookmarkButton from '@/components/BookmarkButton'
 
 export default {
   name: 'BookDetail',
+  components: {
+    'author-widget': AuthorWidget,
+    'bookmark-button': BookmarkButton,
+  },
   computed: {
     book() {
       const id = this.$router.currentRoute._value.params.id
@@ -14,20 +20,48 @@ export default {
 
 <template>
 
-  <div class="navbar-horizontal">
-    <router-link :to="{ name: 'Home' }">&lt; Back to Books</router-link>
-  </div>
+  <div class="content mx-6">
 
-  <div class="columns">
-    <div class="left-column">
-      <div class="book-cover-wrapper">
-        <img class="cover" :src="book.cover"/>
+    <div class="directory-navbar mt-4 mb-5">
+      <div class="is-flex is-justify-content-space-between">
+        <div>
+          <router-link :to="{ name: 'Home' }" class="directory-nav-link is-uppercase">&lt; Back to Books</router-link>
+        </div>
+        <div>
+          <router-link :to="{ name: 'Home' }" class="directory-nav-link is-uppercase mx-6">&lt; Previous Book</router-link>
+          <router-link :to="{ name: 'Home' }" class="directory-nav-link is-uppercase">Next Book &gt;</router-link>
+        </div>
       </div>
     </div>
 
-    <div class="right-column">
-      <h1 class="title">{{book.title}}</h1>
+    <div class="columns">
+      <div class="column is-two-fifths mr-4">
+        <div class="book-cover-wrapper">
+          <img class="cover" :src="book.cover"/>
+        </div>
+      </div>
+
+      <div class="column">
+        <div class="title-container divider-bottom is-flex is-justify-content-space-between">
+          <h1 class="title">{{book.title}}</h1>
+          <div style="padding-top: 0px;"><bookmark-button /></div>
+        </div>
+
+        <div class="authors divider-bottom">
+          <div v-for="person of book.authors" :key="person" class="mb-2">
+            <author-widget :name="person"></author-widget>
+          </div>
+          <div v-for="person of book.illustrators" :key="person">
+            <author-widget :name="person"></author-widget>
+          </div>
+        </div>
+
+        <p>{{book.description}}</p>
+
+      </div>
+
     </div>
+
   </div>
 
 </template>
@@ -35,12 +69,20 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/vars.scss';
 
-.columns {
-  display: flex;
+.directory-nav-link {
+  color: black;
 }
 
-.left-column {
-  max-width: 40%;
+.title {
+  font-size: 3.5rem;
+  line-height: 1;
+  margin-bottom: 0;
+}
+
+.divider-bottom {
+  padding-bottom: 1rem;
+  margin-bottom: 2rem;
+  border-bottom: solid 1px #ddd;
 }
 
 </style>
