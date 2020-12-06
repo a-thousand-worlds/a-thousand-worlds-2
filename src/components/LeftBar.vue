16a12401fb4db2004d39c410195c78cab6a78b0a
Raine Revere <raine@cybersemics.org>
2020-12-06 16:12:07 -0700

    LeftBar: +social icons

--- src/components/LeftBar.vue
+++ src/components/LeftBar.vue
@@ -1,3 +1,15 @@
+<script>
+import InstagramIcon from '../assets/icons/instagram.svg'
+import TwitterIcon from '../assets/icons/twitter.svg'
+
+export default {
+  components: {
+    InstagramIcon,
+    TwitterIcon,
+  },
+}
+</script>
+
 <template>
   <aside class="is-align-self-stretch menu is-flex-direction-column is-justify-content-space-between">
     <div id="logo">
@@ -11,6 +23,9 @@
       <li><router-link :to="{name: 'Instagram'}">Instagram</router-link></li>
       <li><router-link :to="{name: 'Support'}">Support</router-link></li>
       <li><router-link :to="{name: 'About'}">About</router-link></li>
+      <li v-if="!$iam('authorized')"><router-link :to="{name: 'LogIn'}">Log In</router-link></li>
+      <li v-if="$iam('authorized')"><router-link :to="{name: 'Profile'}">Profile</router-link></li>
+      <li v-if="$iam('authorized')"><router-link :to="{name: 'LogOut'}">Log Out</router-link></li>
     </ul>
     <ul v-if="$iam('superadmin')" class="menu-list mt-5">
       <li><router-link :to="{name: 'TagsManager'}">Tags Manager</router-link></li>
@@ -18,16 +33,21 @@
       <li><router-link :to="{name: 'BundlesManager'}">Bundle Manager</router-link></li>
       <li><router-link :to="{name: 'PeopleManager'}">People Manager</router-link></li>
     </ul>
-    <ul class="menu-list mt-5">
-      <li v-if="!$iam('authorized')"><router-link :to="{name: 'LogIn'}">Log In</router-link></li>
-      <li v-if="$iam('authorized')"><router-link :to="{name: 'Profile'}">Profile</router-link></li>
-      <li v-if="$iam('authorized')"><router-link :to="{name: 'LogOut'}">Log Out</router-link></li>
+    <ul class="menu-list mt-50">
+      <li>
+        <a href="https://instagram.com" target="_blank" class="inline"><InstagramIcon/></a>
+        <a href="https://twitter.com" target="_blank" class="inline p-0"><TwitterIcon/></a>
+      </li>
     </ul>
   </aside>
 </template>
 
 <style scoped lang="scss">
 @import '@/assets/vars.scss';
+
+.inline {
+  display: inline;
+}
 
 #logo {
   width: 80px;
