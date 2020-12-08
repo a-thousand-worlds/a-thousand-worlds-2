62e1913871d33374effab3f8f04e5290f9e29dff
Raine Revere <raine@cybersemics.org>
2020-12-07 21:29:30 -0700

    functions: +findISBN

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const searchISBN = require('./searchISBN')
+const findISBN = require('./findISBN')
 
 // increase function memory since we are doing image processing
 // https://firebase.google.com/docs/functions/manage-functions#set_timeout_and_memory_allocation
@@ -7,3 +8,7 @@ const searchISBN = require('./searchISBN')
 exports.searchISBN = functions
   .runWith({ memory: '1GB' })
   .https.onRequest(searchISBN())
+
+exports.findISBN = functions
+  .runWith({ memory: '1GB' })
+  .https.onRequest(findISBN())
