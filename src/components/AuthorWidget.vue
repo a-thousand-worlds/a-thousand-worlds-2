b9a27cf0e23db212afccbd416116d9565f2f10cc
Raine Revere <raine@cybersemics.org>
2020-12-09 15:51:29 -0700

    AuthorWidget: fix fontSize

--- src/components/AuthorWidget.vue
+++ src/components/AuthorWidget.vue
@@ -22,7 +22,7 @@ export default {
 </script>
 
 <template>
-  <div class="mb-3 is-uppercase widget" :style="{'font-size': this.fontSize+'%'}">
+  <div class="mb-3 is-uppercase widget" :style="{'font-size': fontSize+'%'}">
     <i v-if="isAuthor" class="fas fa-pencil-alt"></i>
     <i v-if="!isAuthor" class="fas fa-palette"></i>
     <div v-if="nolink === true" class="name ml-2">{{name}}</div>
