a2bbb675e09030049db4ae607da0d5cd0a49e4de
Raine Revere <raine@cybersemics.org>
2020-12-11 19:31:12 -0700

    +Toggle component to show/hide content on toggle

---
+++ src/components/Toggle.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+  props: ['open'],
+  emits: ['toggle'],
+  data() {
+    return {
+      isOpen: this.open ?? false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+      this.$emit('toggle', this.isOpen)
+    }
+  }
+}
+
+</script>
+
+<template>
+  <a @click.prevent="toggle" style="user-select: none;">
+    <span class="icon is-small mb-10" style="position: relative; top: 1px;">
+      <i class="fas mr-1" :class="'fa-angle-' + (isOpen ? 'down' : 'right')" aria-hidden="true" style="color: black;"></i>
+    </span>
+    <slot name="label">label</slot>
+  </a>
+  <slot name="content" v-if="isOpen">content</slot>
+</template>
+
+<style scoped lang="scss">
+@import '@/assets/main.scss';
+
+</style>
--- src/pages/Invite.vue
+++ src/pages/Invite.vue
@@ -1,15 +1,16 @@
 <script>
 import Content from '@/components/Content'
+import Toggle from '@/components/Toggle'
 
 export default {
   name: 'Invite',
   components: {
     Content,
+    Toggle,
   },
   data() {
     return {
       roles: ['user', 'contributor', 'creator', 'admin', 'superadmin'],
-      showTemplatingHelp: false,
     }
   },
 }
@@ -29,21 +30,26 @@ export default {
 
       <h2 class="divider-bottom">Invitation Email Templates</h2>
 
-      <a @click.prevent="showTemplatingHelp = !showTemplatingHelp"><span class="icon is-small mb-10" style="position: relative; top: 1px;"><i class="fas mr-1" :class="{ 'fa-angle-right': !showTemplatingHelp, 'fa-angle-down': showTemplatingHelp }" aria-hidden="true" style="color: black;"></i></span>Templating Help</a>
+      <Toggle>
+        <template #label>Templating Help</template>
+        <template #content>
 
-      <div class="bg-secondary p-20" v-if="showTemplatingHelp">
+          <div class="bg-secondary p-20">
 
-        <h3>Rich text</h3>
-        <p class="my-10">You can add bold, italic, and other styles to the templates. Just select some text and a popup toolbar will appear.</p>
+            <h3>Rich text</h3>
+            <p class="my-10">You can add bold, italic, and other styles to the templates. Just select some text and a popup toolbar will appear.</p>
 
-        <h3>Template variables</h3>
-        <p class="my-10">You can use special codes in the email templates and they will automatically be replaced with the correct value for the invitee.</p>
-        <p>
-          <b>NAME</b>: The name of the invitee.<br/>
-          <b>LINK</b>: The invitation link for the selected role.
-        </p>
+            <h3>Template variables</h3>
+            <p class="my-10">You can use special codes in the email templates and they will automatically be replaced with the correct value for the invitee.</p>
+            <p>
+              <b>NAME</b>: The name of the invitee.<br/>
+              <b>LINK</b>: The invitation link for the selected role.
+            </p>
 
-      </div>
+          </div>
+
+        </template>
+      </Toggle>
 
       <div v-for="role of roles" :key="role" class="my-30">
         <h3 class="is-capitalized my-10">{{ role === 'superadmin' ? 'owner' : role === 'admin' ? 'advisor' : role }}</h3>
