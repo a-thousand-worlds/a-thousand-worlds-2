4efb003b33107be5688439ea674b60991af0bbfb
Raine Revere <raine@cybersemics.org>
2020-12-18 18:21:32 -0700

    fix vue-svg-loader configuration to allow scaling with viewBox
    vue-svg-loader removes viewBox by default if width and height are present to optimize size
    override to preserve width and height while allowing it to scale properly if width and height are overridden (e.g. set to 100% to fill container)
    https://github.com/visualfanatic/vue-svg-loader/issues/58

--- vue.config.js
+++ vue.config.js
@@ -11,5 +11,13 @@ module.exports = {
       .end()
       .use('vue-svg-loader')
       .loader('vue-svg-loader')
+      // vue-svg-loader removes viewBox by default if width and height are present to optimize size
+      // override to preserve width and height while allowing it to scale properly if width and height are overridden (e.g. set to 100% to fill container)
+      // https://github.com/visualfanatic/vue-svg-loader/issues/58
+      .options({
+        svgo: {
+          plugins: [{ removeViewBox: false }]
+        }
+      })
   },
 }
