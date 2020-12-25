e68148b0ddafda8cc12dfcb308c75c1177a1f60f
Raine Revere <raine@cybersemics.org>
2020-12-24 17:08:31 -0700

    test-helpers: comment

--- src/test-helpers.js
+++ src/test-helpers.js
@@ -3,7 +3,9 @@ import store from '@/store'
 import mixins from '@/mixins'
 import directives from '@/directives'
 
+/** Renders a component with global directives, mixins, and store for tests. */
 export const render = component => vueRender(component, {
+  // https://vue-test-utils.vuejs.org/v2/api/#mount
   global: {
     directives,
     mixins: [mixins],
