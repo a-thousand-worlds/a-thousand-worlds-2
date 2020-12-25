b49df52a97a13faa1352808025b59902faad7628
Raine Revere <raine@cybersemics.org>
2020-12-25 08:16:28 -0700

    jest.config.js: note @vue/cli-plugin-unit-jest fix

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,8 @@ module.exports = {
   ],
   transform: {
     '^.+\\.vue$': 'vue-jest',
+    // fixed in next release of @vue/cli-plugin-unit-jest > 4.5.9
+    // https://github.com/vuejs/vue-cli/pull/6170
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': 'jest-transform-stub'
   },
 }
