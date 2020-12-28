e1b987348a06b629b02a84669826261ba643aaaa
Raine Revere <raine@cybersemics.org>
2020-12-28 12:12:35 -0700

    SubmissionThankYou: default text

--- src/pages/SubmissionThankYou.vue
+++ src/pages/SubmissionThankYou.vue
@@ -1,7 +1,11 @@
 <script>
+import Content from '@/components/Content'
 
 export default {
   name: 'SubmissionThankYou',
+  components: {
+    Content,
+  },
 }
 
 </script>
@@ -11,8 +15,9 @@ export default {
   <div class="my-50 is-flex is-justify-content-center">
     <div class="is-flex-grow-1 mx-20" style="max-width: 760px;">
 
-      <h1>Thank you for your submission!</h1>
-      <div class="has-text-centered my-20">
+      <h1 class="title has-text-centered">Thank you</h1>
+      <div class="has-text-centered my-30">
+        <p class="mb-20" style="font-size: 30px;"><Content name="submission/thankyou">Thank you! A Thousand Worlds will review your submission and reach out if we have questions or once it's been approved.</Content></p>
         <router-link :to="{ name: 'Dashboard' }"><button class="button is-rounded is-primary">Dashboard</button></router-link>
       </div>
 
