36e420fa13c7454b22d6fb43716f297e3db9471d
Raine Revere <raine@cybersemics.org>
2020-12-31 11:16:45 -0700

    BookDetail: tweak footer responsive breakpoints

--- src/pages/BookDetail.vue
+++ src/pages/BookDetail.vue
@@ -121,15 +121,15 @@ export default {
 
   <div v-if="book" class="content-footer">
     <div class="content-footer-inner">
-      <div class="from-fullhd">
-        <input type="text" class="input" style="width: 16rem;" :value="pageUrl" readonly />
-        <button id="copy-link" class="button is-rounded is-secondary mx-10" :data-clipboard-text="pageUrl">COPY LINK</button>
+      <div class="from-widescreen mr-30">
+        <input type="text" class="input from-fullhd" style="width: 16rem;" :value="pageUrl" readonly />
+        <button id="copy-link" class="button is-rounded is-secondary mx-10" :data-clipboard-text="pageUrl">Copy <span class="is-hidden-fullhd mx-1"> Page </span> Link</button>
       </div>
       <div>
-        <button class="button button-unstyled is-primary">FIND BOOK AT</button>
+        <button class="button button-unstyled is-primary pl-0">FIND BOOK AT</button>
         <button class="button is-rounded is-secondary mx-1">LOCAL LIBRARY</button>
         <button class="button is-rounded is-secondary mx-1">LINK</button>
-        <a :href="`http://www.indiebound.org/book/${book.isbn}?aff=athousandworlds`" target="_blank"><button class="button is-rounded is-secondary mx-1">INDIEBOUND</button></a>
+        <a :href="`http://www.indiebound.org/book/${book.isbn}?aff=athousandworlds`" target="_blank"><button class="button is-rounded is-secondary ml-1 mr-20">INDIEBOUND</button></a>
       </div>
     </div>
   </div>
@@ -173,14 +173,13 @@ export default {
   color: $primary-invert;
   background-color: $primary;
   width: calc(100% - #{$leftbar-width} - #{$rightbar-width});
-  padding: 10px 20px;
   white-space: nowrap;
   overflow: auto;
 
   @include until($tablet) {
     left: 0;
     width: 100%;
-    padding-bottom: $mobile-footer-height + 9px;
+    padding-bottom: $mobile-footer-height;
   }
 
   .button, input {
@@ -190,11 +189,15 @@ export default {
 
 .content-footer-inner {
   display: flex;
-  justify-content: center;
-  margin-left: 60px;
-  margin-right: 60px;
+  justify-content: flex-start;
+  margin: 10px 20px;
 
-  @include from($fullhd) {
+  // use custom breakpoint between tablet and desktop to preserve centering when there is room
+  @include from(900px) {
+    margin: 20px 60px;
+    justify-content: center;
+  }
+  @include from($widescreen) {
     justify-content: space-between;
   }
 }
