5a5ace11395072a3b17ea8dc198edf5c3a3fb4f5
Raine Revere <raine@cybersemics.org>
2021-01-01 12:39:15 -0700

    functions/index.js: minor formatting

--- functions/index.js
+++ functions/index.js
@@ -5,12 +5,8 @@ const metadataByISBN = require('./metadataByISBN')
 const searchISBN = require('./searchISBN')
 const watchBookSubmissions = require('./watchBookSubmissions')
 
-// increase function memory since we are doing image processing
-// https://firebase.google.com/docs/functions/manage-functions#set_timeout_and_memory_allocation
-
-exports.searchISBN = functions
-  .runWith({ memory: '1GB' })
-  .https.onRequest(searchISBN())
+exports.coverImageByISBN = functions
+  .https.onRequest(coverImageByISBN())
 
 exports.email = functions
   .https.onRequest(email())
@@ -18,7 +14,11 @@ exports.email = functions
 exports.metadataByISBN = functions
   .https.onRequest(metadataByISBN())
 
-exports.coverImageByISBN = functions
-  .https.onRequest(coverImageByISBN())
+// increase function memory since we are doing image processing
+// https://firebase.google.com/docs/functions/manage-functions#set_timeout_and_memory_allocation
+
+exports.searchISBN = functions
+  .runWith({ memory: '1GB' })
+  .https.onRequest(searchISBN())
 
 exports.watchBookSubmissions = watchBookSubmissions
