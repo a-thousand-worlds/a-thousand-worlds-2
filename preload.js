cffadf7cbca2aee7ef24ce072451484e19e4d913
Raine Revere <raine@cybersemics.org>
2021-01-01 12:48:25 -0700

    +preload script

---
+++ preload.js
@@ -0,0 +1,34 @@
+/*
+
+Preloads Firebase tags into /src/assets/tags.json for loading initial filters faster. Use "npm run preload" instead of calling direcly.
+
+NOT USED. Will consider if the load time is noticeable.
+
+*/
+const fs = require('fs')
+const _ = require('lodash')
+
+let tagsFile
+try {
+  tagsFile = fs.readFileSync('/tmp/atw/tags.json')
+}
+catch (e) {
+  if (e.code === 'ENOENT') {
+    console.error('ERROR: Missing /tmp/atw/tags.json. Run "npm run preload" to download collections from Firebase Realtime Database.')
+    process.exit(1)
+  }
+  throw e
+}
+
+const tagsFull = JSON.parse(tagsFile)
+
+// strip timestamps to reduce payload
+const tags = Object.entries(tagsFull).reduce((accum, [categoryKey, categoryValue]) => ({
+  ...accum,
+  [categoryKey]: Object.entries(categoryValue).reduce((accumInner, [tagKey, tagValue]) => ({
+    ...accumInner,
+    [tagKey]: _.pick(tagValue, ['id', 'showOnFront', 'sortOrder', 'tag'])
+  }), {})
+}), {})
+
+fs.writeFileSync('src/assets/tags.json', JSON.stringify(tags, null, 2))
