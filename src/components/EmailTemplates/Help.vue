392035791cf946a52d80d0d1a19e282ca17d3164
Raine Revere <raine@cybersemics.org>
2021-01-05 10:55:04 -0700

    +EmailTemplates/Help

---
+++ src/components/EmailTemplates/Help.vue
@@ -0,0 +1,24 @@
+<script>
+export default {}
+</script>
+
+<template>
+
+  <div class="bg-secondary p-20">
+
+    <h3>Rich text</h3>
+    <p class="my-10">You can add bold, italic, and other styles to the templates. Just select some text and a popup toolbar will appear.</p>
+
+    <h3>Template variables</h3>
+    <p class="my-10">You can use special codes in the email templates and they will automatically be replaced with the correct value for the invitee.</p>
+    <p>
+      <ul>
+        <li><b>FIRST_NAME</b>: The first name of the invitee.</li>
+        <li><b>LAST_NAME</b>: The last name of the invitee.</li>
+        <li><b>FULL_NAME</b>: The full name of the invitee.</li>
+        <li><b>SIGNUP_LINK</b>: The signup link for the selected role.</li>
+      </ul>
+    </p>
+  </div>
+
+</template>
--- src/pages/EmailTemplates.vue
+++ src/pages/EmailTemplates.vue
@@ -1,10 +1,12 @@
 <script>
 import EmailTemplatesEditor from '@/components/EmailTemplates/Editor'
+import EmailTemplatesHelp from '@/components/EmailTemplates/Help'
 
 export default {
   name: 'EmailTemplates',
   components: {
     EmailTemplatesEditor,
+    EmailTemplatesHelp,
   },
 }
 
@@ -21,24 +23,7 @@ export default {
 
       <h1 class="divider-bottom">Email Templates</h1>
 
-      <h2 class="divider-bottom">Templates</h2>
-      <div class="bg-secondary p-20">
-
-        <h3>Rich text</h3>
-        <p class="my-10">You can add bold, italic, and other styles to the templates. Just select some text and a popup toolbar will appear.</p>
-
-        <h3>Template variables</h3>
-        <p class="my-10">You can use special codes in the email templates and they will automatically be replaced with the correct value for the invitee.</p>
-        <p>
-          <ul>
-            <li><b>FIRST_NAME</b>: The first name of the invitee.</li>
-            <li><b>LAST_NAME</b>: The last name of the invitee.</li>
-            <li><b>FULL_NAME</b>: The full name of the invitee.</li>
-            <li><b>SIGNUP_LINK</b>: The signup link for the selected role.</li>
-          </ul>
-        </p>
-
-      </div>
+      <EmailTemplatesHelp />
 
       <section class="section px-0">
         <EmailTemplatesEditor />
