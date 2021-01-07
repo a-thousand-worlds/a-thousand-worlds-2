8dd746922f1afb4f2cfd1328d7c6a95fdcab5aed
Raine Revere <raine@cybersemics.org>
2021-01-07 16:17:52 -0700

    randomize books (fix)

--- src/store/books.js
+++ src/store/books.js
@@ -1,14 +1,21 @@
+import _ from 'lodash'
 import mergeOne from '@/util/mergeOne'
 import collection from '@/store/collection/imaged'
 
 const module = mergeOne(collection('books', 'cover'), {
   state: {
+    booksOrdered: [],
     filters: [],
   },
   mutations: {
     resetFilters(state) {
       state.filters = []
-      state.booksFiltered = state.booksList
+    },
+    // override module set to randomize books
+    set(state, data) {
+      state.data = data
+      state.booksOrdered = _.shuffle(Object.values(data))
+      state.loaded = true
     },
     toggleFilter(state, filter) {
       if (!state.filters.includes(filter)) {
@@ -21,19 +28,20 @@ const module = mergeOne(collection('books', 'cover'), {
     }
   },
   getters: {
-    filtered: state => state.loaded
-      ? Object.keys(state.data)
-        .map(id => state.data[id])
-        .filter(book => {
-          if (!state.filters.length) return true
-          return state.filters.every(tag => {
-            let list = []
-            if (Array.isArray(book.tags)) list = book.tags
-            else if (typeof book.tags === 'object') list = Object.keys(book.tags)
-            return list.includes(tag)
-          })
+    filtered: state => {
+      if (!state.loaded) return []
+
+      if (state.filters.length === 0) return state.booksOrdered
+
+      return state.booksOrdered.filter(book =>
+        state.filters.every(tag => {
+          const list = Array.isArray(book.tags) ? book.tags :
+            typeof book.tags === 'object' ? Object.keys(book.tags) :
+            []
+          return list.includes(tag)
         })
-      : []
+      )
+    }
   }
 })
 
