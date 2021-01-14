cf6453343a9928cf551bc339e679b08b86ed9882
Raine Revere <raine@cybersemics.org>
2021-01-14 11:17:50 -0700

    YourSubmissions: formatting

--- src/components/Dashboard/YourSubmissions.vue
+++ src/components/Dashboard/YourSubmissions.vue
@@ -36,30 +36,30 @@ export default {
 
 <template>
   <div>
-    <div v-if="pendingSubmissions.length" class="my-20">
+    <div v-if="pendingSubmissions.length">
       <h2>{{ $can('review') ? 'Your ' : '' }}Pending Submissions</h2>
-      <div class="columns is-multiline">
-        <div v-for="sid of pendingSubmissions" :key="sid" class="column is-6-tablet is-4-desktop is-3-widescreen">
+      <div class="is-flex is-flex-wrap-wrap">
+        <div v-for="sid of pendingSubmissions" :key="sid" class="mr-20 mb-20">
           <UserBookSubmission :sid="sid" />
         </div>
       </div>
     </div>
 
-    <div v-if="acceptedSubmissions.length" class="my-20">
+    <div v-if="acceptedSubmissions.length">
       <h2>{{ $can('review') ? 'Your ' : '' }}Accepted Submissions</h2>
-      <div class="columns is-multiline">
-        <div v-for="sid of acceptedSubmissions" :key="sid" class="column is-6-tablet is-4-desktop is-3-widescreen">
+      <div class="is-flex is-flex-wrap-wrap">
+        <div v-for="sid of acceptedSubmissions" :key="sid" class="mr-20 mb-20">
           <UserBookSubmission :sid="sid" />
         </div>
       </div>
     </div>
 
-    <div v-if="rejectedSubmissions.length" class="my-20">
+    <div v-if="rejectedSubmissions.length">
       <Toggle>
         <template #label>Not Approved</template>
         <template #content>
-          <div class="columns is-multiline">
-            <div v-for="sid of rejectedSubmissions" :key="sid" class="column is-6-tablet is-4-desktop is-3-widescreen">
+          <div class="is-flex is-flex-wrap-wrap mt-10">
+            <div v-for="sid of rejectedSubmissions" :key="sid" class="mr-20 mb-20">
               <UserBookSubmission :sid="sid" />
             </div>
           </div>
