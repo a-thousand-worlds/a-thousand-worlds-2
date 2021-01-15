96723c53c7bca423fb971b543ad5acf278748bd8
Raine Revere <raine@cybersemics.org>
2021-01-15 12:41:32 -0700

    ReviewSubmissions/Person

--- src/components/ReviewSubmissions/Person.vue
+++ src/components/ReviewSubmissions/Person.vue
@@ -3,12 +3,10 @@ import _ from 'lodash'
 import Jimp from 'jimp'
 import BalloonEditor from '@ckeditor/ckeditor5-build-balloon'
 
-import MultiPersonField from '@/components/fields/MultiPerson'
 import SimpleInput from '@/components/fields/SimpleInput'
 
 export default {
   components: {
-    MultiPersonField,
     SimpleInput,
   },
   props: ['submission', 'modelValue', 'checked'],
@@ -17,38 +15,32 @@ export default {
       busy: false,
       sub: this.submission || {},
       editor: BalloonEditor,
-      // selected: this.checked || false,
-      // submitter: {},
-      cover: null,
-      people: []
+      image: null,
     }
   },
   computed: {
     ckConfig() {
       return {
         toolbar: [],
-        placeholder: 'No summary'
+        placeholder: 'No bio'
       }
     },
-    coverRatio() {
-      return this.sub.cover?.height / this.sub.cover?.width * 100 || 100
+    imageRatio() {
+      return this.sub.image?.height / this.sub.image?.width * 100 || 100
     },
-    coverUrl() {
+    imageUrl() {
       return this.sub.thumbnail?.startsWith('http')
         ? this.sub.thumbnail
-        : this.sub.cover?.url.startsWith('http')
-          ? this.sub.cover?.url
-          : this.sub.cover?.base64
-            ? this.sub.cover?.base64.startsWith('data:image')
-              ? this.sub.cover?.base64
-              : `data:image/png;base64,${this.sub.cover.base64}`
+        : this.sub.image?.url.startsWith('http')
+          ? this.sub.image?.url
+          : this.sub.image?.base64
+            ? this.sub.image?.base64.startsWith('data:image')
+              ? this.sub.image?.base64
+              : `data:image/png;base64,${this.sub.image.base64}`
             : ''
     },
-    tags() {
-      const tags = this.$store.getters['tags/books/listSorted']()
-        .filter(tag => this.sub.tags && this.sub.tags[tag.id])
-      if (!this.sub.otherTag) return tags
-      return [...tags, { tag: this.sub.otherTag }]
+    identities() {
+      return Object.keys(this.sub?.identities || {})
     }
   },
   watch: {
@@ -57,18 +49,21 @@ export default {
     }
   },
   methods: {
+
     async reject() {
-      await this.$store.commit('ui/setBusy', true)
-      await this.$store.dispatch('submissions/books/reject', this.sub)
-      await this.$store.commit('ui/setBusy', false)
-      this.$store.dispatch('ui/popup', 'Book rejected')
+      this.$store.commit('ui/setBusy', true)
+      await this.$store.dispatch('submissions/people/reject', this.sub)
+      this.$store.commit('ui/setBusy', false)
+      this.$store.dispatch('ui/popup', 'Person submission rejected')
     },
+
     save: _.debounce(async function() {
-      await this.$store.dispatch('submissions/books/save', {
+      await this.$store.dispatch('submissions/people/save', {
         path: this.sub.id,
         value: { ...this.sub }
       })
     }, 500),
+
     fileChange(e) {
       const file = e.target.files[0]
       const reader = new FileReader()
@@ -78,7 +73,7 @@ export default {
             console.error('jimp error', err)
           }
           if (img) {
-            this.sub.cover = {
+            this.sub.image = {
               base64: reader.result,
               height: img.bitmap.width,
               width: img.bitmap.height,
@@ -99,94 +94,54 @@ export default {
 <template>
   <div class="container">
     <div class="columns">
-      <!--
-    <div class="column is-1">
-      <input :disabled="busy" type="checkbox" v-model="selected"/>
-    </div>
-    -->
 
-      <!-- cover -->
+      <!-- image -->
       <div class="column is-2 is-offset-1">
-        <div class="bg-secondary cover-wrapper" :style="{'padding-top': coverRatio +'%', 'background-image': 'url('+coverUrl+')'}">
+        <div class="bg-secondary image-wrapper" :style="{'padding-top': imageRatio +'%', 'background-image': 'url('+imageUrl+')'}">
           <div v-if="busy" class="upload-icon loading">
             <i class="fas fa-spinner fa-pulse fa-fw" />
           </div>
-          <label v-if="!busy" class="upload-icon" for="cover-upload">
+          <label v-if="!busy" class="upload-icon" for="image-upload">
             <i class="fas fa-file-upload fa-fw" />
           </label>
-          <input id="cover-upload" type="file" class="cover-file-uploader" @change.prevent="fileChange($event)">
+          <input id="image-upload" type="file" class="image-file-uploader" @change.prevent="fileChange($event)">
         </div>
       </div>
 
-      <!-- title, authors, illustrators -->
+      <!-- name -->
       <div class="column is-3">
 
-        <div style="font-weight: bold;">
+        <div>
+          <SimpleInput
+            v-model="sub.name"
+            @update:modelValue="save"
+            :disabled="busy"
+            style="font-weight: bold;"
+            placeholder="Name"
+          />
           <SimpleInput
             v-model="sub.title"
-            @update:modelValue="save()"
+            @update:modelValue="save"
             :disabled="busy"
-            :placeholder="'Title'" />
+            placeholder="Title"
+          />
         </div>
-        <div>
-          <MultiPersonField
-            v-model="sub.authors"
-            @update:modelValue="save()"
-            :disabled="busy"
-            :role="'author'"
-            :placeholder="'author(s)'"
-            :search-db="false"
-            pre-text="by" />
-        </div>
-        <div>
-          <MultiPersonField
-            v-model="sub.illustrators"
-            @update:modelValue="save()"
-            :disabled="busy"
-            :role="'illustrator'"
-            :placeholder="'illustrator(s)'"
-            :search-db="false"
-            pre-text="illustrated by" />
-        </div>
-        <div>
-          <SimpleInput
-            v-model="sub.year"
-            @update:modelValue="save()"
-            :disabled="busy"
-            :placeholder="'Year'" />
-        </div>
-      <!--
-      <div>
-        <SimpleInput
-          :disabled="busy"
-          :placeholder="'ISBN'"
-            @update:modelValue="save()"
-          v-model="sub.isbn"/>
-      </div>
-      <div>
-        <SimpleInput
-          :disabled="busy"
-          :placeholder="'Publisher'"
-            @update:modelValue="save()"
-          v-model="sub.publisher"/>
-      </div>
-      -->
       </div>
 
-      <div class="column is-5" style="margin-top: -20px;">
-        <!-- summary -->
+      <div class="column is-5">
+        <!-- bio -->
         <div>
-          <ckeditor @update:modelValue="save()" v-model="sub.summary" :disabled="busy" class="oneline" :editor="editor" :config="ckConfig" style="padding: 0;" />
+          <ckeditor @update:modelValue="save" v-model="sub.bio" :disabled="busy" class="oneline" :editor="editor" :config="ckConfig" style="padding: 0;" />
         </div>
-        <!-- tags -->
+        <!-- identities -->
         <div class="tags">
-          <div v-for="(tag, tagi) of tags" :key="tagi" class="button is-primary is-rounded is-mini mr-1 mb-1" style="cursor: default;">{{ tag.tag }}</div>
+          <div v-for="identity of identities" :key="identity" class="button is-primary is-rounded is-mini mr-1 mb-1" style="cursor: default;">{{ identity }}</div>
         </div>
       </div>
 
       <!-- reject -->
       <div class="column is-1 has-text-right">
-        <button :class="{disabled:busy}" :disabled="busy" class="is-flat is-uppercase is-underlined" @click="reject()">
+        <button :class="{ disabled: busy }" :disabled="busy" title="Reject submission" class="is-flat is-uppercase is-underlined" @click="reject">
           <i class="fas fa-times" style="font-size: 20px;" />
         </button>
       </div>
@@ -202,22 +157,14 @@ export default {
   margin-top: 2rem;
 }
 
-h3.title {
-  font-size: 110%;
-}
-
 input[type="checkbox"] {
   width: 25px;
   height: 25px;
 }
 
-.ck-read-only {
-  color: #aaa;
-}
-
-.cover-wrapper {
+.image-wrapper {
   width: 100%;
-  background-size: cover;
+  background-size: image;
   position: relative;
 
   .upload-icon {
@@ -241,20 +188,8 @@ input[type="checkbox"] {
 
 }
 
-#cover-upload {
+#image-upload {
   display: none;
 }
 
-.columns2 {
-  column-count: 2;
-
-  .label {
-    position: relative;
-    top: -7px;
-  }
-
-  .input.is-inline {
-    margin-top: -7px;
-  }
-}
 </style>
