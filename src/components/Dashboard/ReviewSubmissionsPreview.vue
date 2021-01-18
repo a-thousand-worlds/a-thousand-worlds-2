6299709fe1b5a87780b06a95e916b783ed772f3c
Raine Revere <raine@cybersemics.org>
2021-01-18 13:17:29 -0700

    +Square component

--- src/components/Dashboard/ReviewSubmissionsPreview.vue
+++ src/components/Dashboard/ReviewSubmissionsPreview.vue
@@ -1,9 +1,11 @@
 <script>
 import BundlesLayoutIcon from '@/assets/icons/bundles-layout.svg'
+import Square from '@/components/Square'
 
 export default {
   components: {
     BundlesLayoutIcon,
+    Square,
   },
   computed: {
     bookSubmissions() {
@@ -32,51 +34,25 @@ export default {
   <div class="columns">
     <div class="column">
       <router-link :to="{ name: 'ReviewSubmissions', params: { type: 'books' } }">
-        <div class="square bg-secondary is-flex is-justify-content-center is-align-items-center">
-          <h3 style="color: black;">Books ({{ bookSubmissions.length }})</h3>
-        </div>
+        <Square>
+          <h3>Books ({{ bookSubmissions.length }})</h3>
+        </Square>
       </router-link>
     </div>
     <div class="column">
       <router-link :to="{ name: 'ReviewSubmissions', params: { type: 'people' } }">
-        <div class="square bg-secondary is-flex is-justify-content-center is-align-items-center" style="border-radius: 999px;">
-          <h3 style="color: black;">People ({{ Object.keys(peopleSubmissions).length }})</h3>
-        </div>
+        <Square style="border-radius: 999px;">
+          <h3>People ({{ Object.keys(peopleSubmissions).length }})</h3>
+        </Square>
       </router-link>
     </div>
     <div class="column">
       <router-link :to="{ name: 'ReviewSubmissions', params: { type: 'bundle' } }">
-        <div class="square bg-secondary is-flex is-justify-content-center is-align-items-center">
+        <Square>
           <BundlesLayoutIcon class="fill-primary" style="position: absolute; opacity: 0.1; width: 90%; height: 100%;" />
-          <h3 style="position: relative; color: black;">Bundles ({{ bundleSubmissions.length }})</h3>
-        </div>
+          <h3>Bundles ({{ bundleSubmissions.length }})</h3>
+        </Square>
       </router-link>
     </div>
   </div>
 </template>
-
-<style scoped lang="scss">
-@import "bulma/sass/utilities/_all.sass";
-
-.square {
-  position: relative;
-  padding: 20px;
-  white-space: nowrap;
-
-  // shrink text on tablet
-  // not mobile since the container becomes big again when stacked
-  @include tablet-only {
-    h3 {
-      font-size: 20px;
-    }
-  }
-
-}
-
-.square:after {
-  content: "";
-  display: block;
-  padding-bottom: 100%;
-}
-
-</style>
---
+++ src/components/Square.vue
@@ -0,0 +1,41 @@
+<!-- A square component that expands to fit while maintaining a 1:1 aspect ratio. -->
+
+<template>
+
+  <div class="square bg-secondary is-flex is-justify-content-center is-align-items-center">
+    <slot />
+  </div>
+
+</template>
+
+<style scoped lang="scss">
+@import "bulma/sass/utilities/_all.sass";
+@import '@/assets/style/mixins.scss';
+
+.square {
+  color: black;
+  position: relative;
+  padding: 20px;
+  white-space: nowrap;
+
+  &:hover {
+    @include primary(color);
+  }
+
+  // shrink text on tablet
+  // not mobile since the container becomes big again when stacked
+  @include tablet-only {
+    h3 {
+      font-size: 20px;
+    }
+  }
+
+}
+
+.square:after {
+  content: "";
+  display: block;
+  padding-bottom: 100%;
+}
+
+</style>
