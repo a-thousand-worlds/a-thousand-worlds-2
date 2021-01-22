21fa6ec89a49716122add94f4bad7f6ac9199c84
Raine Revere <raine@cybersemics.org>
2021-01-22 16:17:40 -0700

    PeopleSubmissionForm: sort identities by tag rank

--- src/pages/PeopleSubmissionForm.vue
+++ src/pages/PeopleSubmissionForm.vue
@@ -27,7 +27,7 @@ export default {
       return this.$store.state.user.user?.profile.draftPerson
     },
     identityOptions() {
-      return this.$store.state.tags.people.data
+      return this.$store.getters[`tags/people/listSorted`]()
     },
     person() {
       const peopleSubmissions = this.$store.state.submissions.people.data || {}
