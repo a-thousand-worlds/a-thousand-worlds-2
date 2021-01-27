6cc76b5d2a52af35d75f1a6f8cf992be142df18c
Raine Revere <raine@cybersemics.org>
2021-01-27 09:37:42 -0700

    util/get: +support for lone "/"

--- src/util/get.test.js
+++ src/util/get.test.js
@@ -11,3 +11,9 @@ test('using "/" delimeter', () => {
   expect(get(o, 'a/b'))
     .toBe(1)
 })
+
+test('support lone "/"', () => {
+  const o = { a: { b: 1 } }
+  expect(get(o, '/'))
+    .toBe(o)
+})
