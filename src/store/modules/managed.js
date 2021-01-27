722fe0a8385e8beb2f08af18691f415eac98902b
Raine Revere <raine@cybersemics.org>
2021-01-27 16:53:49 -0700

    modules/manager: fix for non-root paths

--- src/store/modules/managed.js
+++ src/store/modules/managed.js
@@ -4,28 +4,38 @@ import mergeOne from '@/util/mergeOne'
 import firebase from '@/firebase'
 import dayjs from 'dayjs'
 
-const module = name => mergeOne(collection(name), {
-  actions: {
-    /** save method overrides parents collection/abstract::save */
-    save(state, { path, value }) {
-      if (!path) throw new Error('path required')
-      // managed collection element should be object
-      if (typeof value !== 'object') throw new Error('value should be object')
+const module = name => {
+  const collectionModule = collection(name)
+  const managedModule = {
+    actions: {
+      /** save method overrides parents collection/abstract::save */
+      save(state, { path, value }) {
+        if (!path) throw new Error('path required')
 
-      const now = dayjs().format()
-      const userId = state.rootState?.user?.user?.uid || null
+        // only manage timestamps if saving a whole collection item
+        if (path !== '/' && path !== '.') {
+          return collectionModule.actions.save(state, { path, value })
+        }
 
-      return firebase.database().ref(`${name}/${path}`)
-        .set({
-          ...value,
-          updatedAt: now,
-          updatedBy: userId,
-          ...!value.createdAt && { createdAt: now },
-          ...!value.createdBy && { createdBy: userId },
-          ...value.reviewedBy && !value.reviewedAt && { reviewedAt: userId },
-        })
+        // managed collection element should be object
+        if (typeof value !== 'object') throw new Error('value should be object')
+
+        const now = dayjs().format()
+        const userId = state.rootState?.user?.user?.uid || null
+
+        return firebase.database().ref(`${name}/${path}`)
+          .set({
+            ...value,
+            updatedAt: now,
+            updatedBy: userId,
+            ...!value.createdAt && { createdAt: now },
+            ...!value.createdBy && { createdBy: userId },
+            ...value.reviewedBy && !value.reviewedAt && { reviewedAt: userId },
+          })
+      }
     }
   }
-})
+  return mergeOne(collectionModule, managedModule)
+}
 
 export default module
