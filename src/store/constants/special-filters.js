00f50eaf2a495dac1859f6f382db76a71df3f543
Raine Revere <raine@cybersemics.org>
2021-01-29 16:11:32 -0700

    Filter: hard-code author/illustrator

---
+++ src/store/constants/special-filters.js
@@ -0,0 +1,12 @@
+// hardcoded special filters that are rendered above tags
+const specialFilters = {
+  books: [],
+  bundles: [],
+  people: [
+    { id: 'author-illustrator', tag: 'Author/Illustrator' },
+    { id: 'author', tag: 'Author' },
+    { id: 'illustrator', tag: 'Illustrator' },
+  ],
+}
+
+export default specialFilters
