1c8c07997cf20144dc8212832602116d7eab27d3
abrakadobr <abrakadobr@gmail.com>
2021-02-12 13:43:32 +0700

    email functions authentication

--- functions/email.js
+++ functions/email.js
@@ -1,5 +1,7 @@
 const admin = require('firebase-admin')
 const express = require('express')
+// cors module automatically handles OPTIONS requests
+const cors = require('cors')({ origin: true })
 const uid = require('uuid/v4')
 const serviceAccount = require('./serviceAccountKey.json')
 
@@ -23,31 +25,67 @@ module.exports = () => {
     credential: admin.credential.cert(serviceAccount),
   }
   admin.initializeApp(adminConfig)
-  const db = admin.firestore()
+  // generates error 9 FAILED_PRECONDITION: The Cloud Firestore API is not available for Datastore Mode projects
+  // const db = admin.firestore()
   const app = express()
+  app.use(cors)
 
   // TODO: Secure this endpoint
   app.get('/', handleError(async (req, res) => {
 
-    res.header('Access-Control-Allow-Origin', '*')
-
-    const { to, html, subject } = req.query
-
-    if (!to || !html || !subject) {
-      res.status(500).send('Insufficient query parameters. to, subject, and html required.')
+    console.log('GET header authorization: ', req.headers.authorization)
+    if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+      res.status(401).send('Not authorized.')
+      return
     }
 
-    const id = uid()
-    await db.collection('mail').add({
-      to,
-      message: {
-        subject,
-        html,
-      }
-    })
+    const token = req.headers.authorization.split('Bearer ')[1]
+    let user = null
+    try {
+      user = await admin.auth().verifyIdToken(token)
+    }
+    catch (err) {
+      console.log('verifying token error', err)
+      user = null
+    }
 
-    console.log(`Email sent to ${to}: "${subject}"`)
-    res.status(200).json({ id })
+    if (!user) {
+      res.status(401).send('Not authorized.')
+      return
+    }
+
+    admin.database().ref(`users/${user.uid}/roles`).once('value', rolesSnap => {
+      const roles = rolesSnap.val()
+
+      // here is a point we testing user permission to send emails
+      if (!roles.owner && !roles.advizor && !roles.contributor && !roles.creator) {
+        console.log('user attempt to send email without permission!', user)
+        res.status(403).send('Not allowed.')
+        return
+      }
+
+      const { to, html, subject } = req.query
+
+      if (!to || !html || !subject) {
+        res.status(500).send('Insufficient query parameters. to, subject, and html required.')
+        return
+      }
+
+      const id = uid()
+      // generates error 9 FAILED_PRECONDITION: The Cloud Firestore API is not available for Datastore Mode projects
+      /*
+      await db.collection('mail').add({
+        to,
+        message: {
+          subject,
+          html,
+        }
+      })
+      */
+
+      console.log(`Email sent to ${to}: "${subject}"`)
+      res.status(200).json({ id })
+    })
 
   }))
 
