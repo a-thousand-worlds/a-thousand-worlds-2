39df0ee4463d211b24b706ebeb014c6be715d5c3
Raine Revere <raine@cybersemics.org>
2021-09-19 10:07:58 -0600

    chronouid: Safe subtraction.

--- src/util/chronouid.js
+++ src/util/chronouid.js
@@ -1,8 +1,9 @@
 import { v4 as uid } from 'uuid'
 
-const decamillenium = new Date('10000-01-01:00:00Z')
+// use numeric timestamp since +new Date('10000-01-01:00:00Z') coerces to NaN on some systems (?)
+const decamillenium = 253402304400000
 
 /** Returns a 20 character universally unique identifier which lexicographic sorts in reverse chronological order. */
-const chronouid = () => `${(decamillenium - new Date()).toString(16)}-${uid().slice(0, 7)}`
+const chronouid = () => `${(decamillenium - Date.now()).toString(16)}-${uid().slice(0, 7)}`
 
 export default chronouid
