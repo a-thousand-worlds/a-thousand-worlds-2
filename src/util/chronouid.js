ad78c340920ac1ba7a8d5cc51a523963c35dd65e
Raine Revere <raine@cybersemics.org>
2021-09-23 13:08:58 -0600

    chronouid: Optional date argument

--- src/util/chronouid.js
+++ src/util/chronouid.js
@@ -4,6 +4,7 @@ import { v4 as uid } from 'uuid'
 const decamillenium = 253402304400000
 
 /** Returns a 20 character universally unique identifier which lexicographic sorts in reverse chronological order. */
-const chronouid = () => `${(decamillenium - Date.now()).toString(16)}-${uid().slice(0, 7)}`
+const chronouid = date =>
+  `${(decamillenium - (date ? date.getTime() : Date.now())).toString(16)}-${uid().slice(0, 7)}`
 
 export default chronouid
