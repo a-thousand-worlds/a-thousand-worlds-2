562cc07aa25e02991de4fecfbef142995d7d3f82
Raine Revere <raine@cybersemics.org>
2021-09-25 10:21:42 -0600

    Fix parseNames return type.

--- src/util/parseNames.js
+++ src/util/parseNames.js
@@ -1,10 +1,10 @@
-/** Parses a user-provided string that is expected to contain one or more people's names and returns an array of trimmed values with conjugations and punctuation removed. Returns an empty string if the input is null or undefined. */
+/** Parses a user-provided string that is expected to contain one or more people's names and returns an array of trimmed values with conjugations and punctuation removed. Returns an empty array if the input is null or undefined. */
 const parseNames = s =>
   s
     ? s
         .split(/[,;&]|\s+and\s+/g)
         .map(x => x && x.trim())
         .filter(x => x)
-    : ''
+    : []
 
 export default parseNames
