9329121af7ae4a8e218e05b6f0cd0bf39507215b
Raine Revere <raine@cybersemics.org>
2021-09-27 16:57:50 -0600

    almostEqual: Ignore diacritics.

--- src/util/almostEqual.js
+++ src/util/almostEqual.js
@@ -1,7 +1,9 @@
+import { remove as diacritics } from 'diacritics'
+
 /** Eliminates non-letter characters for almostEqual comparisons. */
 const normalize = s => (s || '').replace(/[\W]/g, '').toLowerCase()
 
-/** Returns true if two strings are a match ignoring spacing and punctuation. */
-const almostEqual = (a, b) => normalize(a) === normalize(b)
+/** Returns true if two strings are a match ignoring spacing, punctuation, and diacritics. */
+const almostEqual = (a, b) => normalize(diacritics(a)) === normalize(diacritics(b))
 
 export default almostEqual
